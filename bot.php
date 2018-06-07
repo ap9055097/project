54954b3402af0267747b1ef7a7a2991f8441b137
Peerapat Kaewdee <ap09055097@gmail.com>
2018-06-07 11:07:16 +0700

    test

--- bot.php
+++ bot.php
@@ -1,6 +1,9 @@
 
 <?php
-$access_token = 'bwLr+NAIExfS1R/pc0NkTVvsi8ftgrwAwuwy3zCtomp3ZuBAYvqb6G7Xf6ZSxoTSkmCluVXDjE+Iu5TFZY2sGa0ZrsZ664ST/bf+AnDEUxxXH8jHGz7Cepf3tfDE/5vyPnYV1NXjTdBpoj31fsJPbAdB04t89/1O/w1cDnyilFU=';
+require "vendor/autoload.php";
+require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
+
+$access_token = '3Fa9KNGEYRuURJ/p3qa4n0BR5cOeDm1N2Wz4eXTo6oUwkybau5B62CaN125LpY8iwPDtfeH8LA5H0wxDa9AMgoPxKVtyd2WyXq2M2MGsXfqJwXVdrkVab6jNctxT4Pp1l0xt+v1fyx/LCyKIajBAzAdB04t89/1O/w1cDnyilFU=';
 
 
 // Get POST body content
