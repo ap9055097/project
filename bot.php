c76929bc9a6e91d9591963e6766c9e616f5005bf
Peerapat Kaewdee <ap09055097@gmail.com>
2018-06-07 11:21:23 +0700

    ess

--- bot.php
+++ bot.php
@@ -24,7 +24,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+				'text' =>  'ping'//$text
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
