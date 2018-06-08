8d65826acc1516b488bac02065a4075d48da05ce
Peerapat Kaewdee <ap09055097@gmail.com>
2018-06-08 10:26:34 +0700

    connect DB

--- bot.php
+++ bot.php
@@ -1,10 +1,7 @@
 
 <?php
-require "vendor/autoload.php";
-require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
-
-$access_token = '3Fa9KNGEYRuURJ/p3qa4n0BR5cOeDm1N2Wz4eXTo6oUwkybau5B62CaN125LpY8iwPDtfeH8LA5H0wxDa9AMgoPxKVtyd2WyXq2M2MGsXfqJwXVdrkVab6jNctxT4Pp1l0xt+v1fyx/LCyKIajBAzAdB04t89/1O/w1cDnyilFU=';
-
+$access_token = 'bwLr+NAIExfS1R/pc0NkTVvsi8ftgrwAwuwy3zCtomp3ZuBAYvqb6G7Xf6ZSxoTSkmCluVXDjE+Iu5TFZY2sGa0ZrsZ664ST/bf+AnDEUxxXH8jHGz7Cepf3tfDE/5vyPnYV1NXjTdBpoj31fsJPbAdB04t89/1O/w1cDnyilFU=';
+require("connection.php");
 
 // Get POST body content
 $content = file_get_contents('php://input');
@@ -21,6 +18,9 @@ if (!is_null($events['events'])) {
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
+			$query="INSERT INTO input_message ('message','receiver') Value (".$text.",'Jobmyway')";
+			$result=mysql_query($query);
+			
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
---
+++ connection.php
@@ -0,0 +1,5 @@
+<?php
+mysql_connect("31.220.110.22","u662042545_exc","ac81c8b1%");
+mysql_select_db("u662042545_chat");
+mysql_query("SET NAMES 'utf8' COLLATE 'utf8_general_ci';");
+?>
