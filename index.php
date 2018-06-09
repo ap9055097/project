0c9c58eaa51b6a6da76cc5c2b65264644c19becd
Peerapat Kaewdee <ap09055097@gmail.com>
2018-06-10 01:01:51 +0700

    test

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php
-echo "Hello LINE BOT";
+echo "Hello LINE BOT<br>";
 
 //require("connection.php");
+
+$text_to_segment = trim('สามารถทำงานได้กับทุกระบบปฏิบัติการ');
+include(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'THSplitLib/segment.php');
+$segment = new Segment();
+//echo '<hr/>';
+$result = $segment->get_segment_array($text_to_segment);
+$test = implode(',', $result);
+echo $test;
