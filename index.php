dcde34a7d820cc2c52e0a4c1cb038c1d699f511b
Peerapat Kaewdee <ap09055097@gmail.com>
2018-06-28 10:03:57 +0700

    line1

--- index.php
+++ index.php
@@ -10,26 +10,7 @@ echo "Hello LINE BOT<br>";
 <head>
 </head>
 <body>
-<div class="fb-customerchat" page_id="185117042183190"></div>
-ssss
-<script>
-  window.fbAsyncInit = function() {
-    FB.init({
-      appId            : '214049609220025',
-      autoLogAppEvents : true,
-      xfbml            : true,
-      version          : 'v3.0'
-    });
-  };
 
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "https://connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
-</script>
 sasasasasssaas
 <!-- Load Facebook SDK for JavaScript -->
 <div id="fb-root"></div>
